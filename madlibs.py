31781c834d0cb504f3c52dfff077cc48b2bec718
corhelms <64820148+CoryHelms@users.noreply.github.com>
2022-01-03 15:46:49 -0500

    Madlibs simple example
    Remembering how to do simple string manipulation while using placeholders.

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+#madlibs game
+noun = input("Enter a noun: ")
+adj = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+famousPerson = input("Name a famous person: ")
+
+madlib = f"After hiding the painting in his {noun} for two years, he grew {adj} and\
+    tried to {verb} it to {famousPerson} in Florence, but was caught."
+
+print(madlib)
