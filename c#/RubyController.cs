8e0ec211144eaa25376e71cda3685522b3ae2901
corhelms <64820148+CoryHelms@users.noreply.github.com>
2022-01-24 13:03:31 -0500

    Adding file backup for game scripts
    These files are current through World Interactions - Damage Zones and Enemies section.

--- c#/RubyController.cs
+++ c#/RubyController.cs
@@ -4,33 +4,68 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
+  public float speed = 3.0f;
 
-    // Start is called before the first frame update
-    void Start()
+  public int maxHealth = 5;
+  public float timeInvincible = 2.0f;
+
+  public int health { get { return currentHealth; }}
+  int currentHealth;
+
+  bool isInvincible;
+  float invincibleTimer;
+
+  Rigidbody2D rigidbody2d;
+  float horizontal;
+  float vertical;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rigidbody2d = GetComponent<Rigidbody2D>();
+
+    currentHealth = maxHealth;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //setting variables for horizontal and vertical movement
+    horizontal = Input.GetAxis("Horizontal");
+    vertical = Input.GetAxis("Vertical");
+
+    if (isInvincible)
     {
-      rigidbody2d = GetComponent<Rigidbody2D>();
+      invincibleTimer -= Time.deltaTime;
+      if (invincibleTimer < 0)
+        isInvincible = false;
+    }
+  }
+
+  void FixedUpdate() 
+  {
+    //Vector2 data type stores x and y coordinates (2d object; No Z-value needed)
+    Vector2 position = rigidbody2d.position;
+
+    //handling movement for x and y coordinates
+    position.x = position.x + speed * horizontal * Time.deltaTime;
+    position.y = position.y + speed * vertical * Time.deltaTime;
+
+    rigidbody2d.MovePosition(position);  
+  }
+
+  public void ChangeHealth(int amount)
+  {
+    if (amount < 0)
+    {
+      if (isInvincible)
+        return;
+      
+      isInvincible = true;
+      invincibleTimer = timeInvincible;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-      //setting variables for horizontal and vertical movement
-      horizontal = Input.GetAxis("Horizontal");
-      vertical = Input.GetAxis("Vertical");
-    }
-
-    void FixedUpdate() 
-    {
-      //Vector2 data type stores x and y coordinates (2d object; No Z-value needed)
-      Vector2 position = rigidbody2d.position;
-
-      //handling movement for x and y coordinates
-      position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-      position.y = position.y + 3.0f * vertical * Time.deltaTime;
-
-      rigidbody2d.MovePosition(position);  
-    }
+    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    Debug.Log(currentHealth + "/" + maxHealth);
+  }
 }
